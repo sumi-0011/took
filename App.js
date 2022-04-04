75b34a3b3a9e05ce48ca1a61021071aa5742e741
sumi.byun <selina2000@naver.com>
2022-04-04 22:06:13 +0900

    feat : navigation example 생성

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
-
-function DetailsScreen() {
+function DetailsScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
-
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -33,4 +45,5 @@ function App() {
     </NavigationContainer>
   );
 }
+
 export default App;
