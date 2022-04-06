47e9587eb4d198360192226d7114163566398ad1
sumi.byun <selina2000@naver.com>
2022-04-07 00:30:28 +0900

    feat : onPress event 추가
    Pressable component이용!

--- src/components/Main/MainBox.tsx
+++ src/components/Main/MainBox.tsx
@@ -1,4 +1,4 @@
-import {Box, Text} from 'native-base';
+import {Box, Pressable, Text} from 'native-base';
 import React from 'react';
 import styled from 'styled-components/native';
 import ImageBg from '../ImageBg';
@@ -7,25 +7,28 @@ type Props = {
   sub: string;
   main: string;
   img: string;
+  onPress?: any;
 };
 
-const MainBox = ({sub, main, img}: Props) => {
+const MainBox = ({sub, main, img, onPress}: Props) => {
   return (
     <Container flex={1} rounded="md" shadow={3} overflow="hidden">
-      <ImageBg img={img}>
-        <Box
-          flex={1}
-          justifyContent="flex-end"
-          alignItems="center"
-          paddingBottom={5}>
-          <Text fontSize="sm" color={'#fff'} bold>
-            {sub}
-          </Text>
-          <Text fontSize="md" color={'#fff'} bold>
-            {main}
-          </Text>
-        </Box>
-      </ImageBg>
+      <Pressable onPress={onPress}>
+        <ImageBg img={img}>
+          <Box
+            flex={1}
+            justifyContent="flex-end"
+            alignItems="center"
+            paddingBottom={5}>
+            <Text fontSize="sm" color={'#fff'} bold>
+              {sub}
+            </Text>
+            <Text fontSize="md" color={'#fff'} bold>
+              {main}
+            </Text>
+          </Box>
+        </ImageBg>
+      </Pressable>
     </Container>
   );
 };
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NativeBaseProvider, Box, HStack, VStack} from 'native-base';
+import {NativeBaseProvider, Box, HStack, VStack, Pressable} from 'native-base';
 // import ProfileBox from 'components/ProfileBox';
 import {
   MainBox,
@@ -10,7 +10,7 @@ import {
 
 type Props = {};
 
-function Main() {
+function Main({navigation}: any) {
   return (
     <NativeBaseProvider>
       <Box
@@ -26,11 +26,13 @@ function Main() {
               sub="지도에서"
               main="쓰레기통 찾기"
               img="https://media.istockphoto.com/vectors/abstract-city-map-simple-map-illustration-vector-id685869052?s=612x612"
+              onPress={() => navigation.navigate('Details')}
             />
             <MainBox
               sub="쓰레기통"
               main="위치 등록하기"
               img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSm2-zWd0cNNhehI_h34ZMZPK0Qj9tf8l39w&usqp=CAU"
+              onPress={() => navigation.navigate('Details')}
             />
           </HStack>
           <NearByTrashBox />
