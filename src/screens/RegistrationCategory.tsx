c60c245ee454da30bb6c6e2f9cabde2338a5e14c
sumi.byun <selina2000@naver.com>
2022-04-11 01:19:29 +0900

    feat : category container

--- src/screens/RegistrationCategory.tsx
+++ src/screens/RegistrationCategory.tsx
@@ -5,8 +5,10 @@ import {
   Text,
   Stack,
   FlatList,
+  Checkbox,
 } from 'native-base';
 import React, {useState} from 'react';
+import BasicButton from '~/components/BasicButton';
 
 type Props = {};
 const categoryList = [
@@ -30,23 +32,77 @@ const categoryList = [
     key: 'food-waste',
     check: false,
   },
+  {
+    name: '잡병',
+    key: 'mixed-bottle',
+    check: false,
+  },
+  {
+    name: '유리병',
+    key: 'glass-bottle',
+    check: false,
+  },
+  {
+    name: '스티로폼',
+    key: 'styrofoam',
+    check: false,
+  },
+  {
+    name: '캔류',
+    key: 'can',
+    check: false,
+  },
+  {
+    name: '헌옷수거함',
+    key: 'old-clothes-locker',
+    check: false,
+  },
+  {
+    name: '고철',
+    key: 'iron',
+    check: false,
+  },
+  {
+    name: '건전지',
+    key: 'battery',
+    check: false,
+  },
+  {
+    name: '형광등',
+    key: 'lamp',
+    check: false,
+  },
 ];
 const RegistrationCategory = () => {
   const [category, setcategory] = useState(categoryList);
   return (
     <NativeBaseProvider>
-      <Text>RegistrationCategory</Text>
-      <Box />
-      <Box>
-        <Input size="lg" placeholder="lg Input" />
-        <FlatList
-          numColumns={2}
-          data={category}
-          renderItem={({item}) => <Box>{item.name}</Box>}
-        />
+      <Box height={'100%'}>
+        <Box flex={1} width={'100%'}>
+          <Text>Hello</Text>
+        </Box>
+        <Box p={5} bg={'#fff'}>
+          <Input size="lg" placeholder="lg Input" marginY={2} />
+          <FlatList
+            numColumns={2}
+            data={category}
+            renderItem={({item}) => (
+              <CheckInput name={item.name} checked={item.check} />
+            )}
+          />
+          <BasicButton>사진 촬영</BasicButton>
+        </Box>
       </Box>
     </NativeBaseProvider>
   );
 };
-
+const CheckInput = ({name, checked}: {name: string; checked: boolean}) => {
+  return (
+    <Box flex={1} paddingY={2}>
+      <Checkbox value={name} defaultIsChecked={checked}>
+        {name}
+      </Checkbox>
+    </Box>
+  );
+};
 export default RegistrationCategory;
