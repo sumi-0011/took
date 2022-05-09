1cc5c1ff367610c7a916c5ac38a2b4195ec3ca18
sumi.byun <selina2000@naver.com>
2022-05-09 21:47:54 +0900

    feat : checkbox 조작 추가

--- src/screens/RegistrationCategory.tsx
+++ src/screens/RegistrationCategory.tsx
@@ -1,97 +1,28 @@
-import {
-  Box,
-  Input,
-  NativeBaseProvider,
-  Text,
-  Stack,
-  FlatList,
-  Checkbox,
-  FormControl,
-} from 'native-base';
+import {Box, Input, NativeBaseProvider, Text} from 'native-base';
 import React, {useState} from 'react';
 import BasicButton from '~/components/BasicButton';
+import CategotyCheckbox from '~/components/Registraion/CategotyCheckbox';
 
+export interface ICategory {
+  name: string;
+  key: string;
+}
 type Props = {};
-const categoryList = [
-  {
-    name: '일반쓰레기',
-    key: 'general-waste',
-    check: false,
-  },
-  {
-    name: '플라스틱',
-    key: 'plastic',
-    check: false,
-  },
-  {
-    name: '종이류',
-    key: 'paper',
-    check: false,
-  },
-  {
-    name: '음식물쓰레기',
-    key: 'food-waste',
-    check: false,
-  },
-  {
-    name: '잡병',
-    key: 'mixed-bottle',
-    check: false,
-  },
-  {
-    name: '유리병',
-    key: 'glass-bottle',
-    check: false,
-  },
-  {
-    name: '스티로폼',
-    key: 'styrofoam',
-    check: false,
-  },
-  {
-    name: '캔류',
-    key: 'can',
-    check: false,
-  },
-  {
-    name: '헌옷수거함',
-    key: 'old-clothes-locker',
-    check: false,
-  },
-  {
-    name: '고철',
-    key: 'iron',
-    check: false,
-  },
-  {
-    name: '건전지',
-    key: 'battery',
-    check: false,
-  },
-  {
-    name: '형광등',
-    key: 'lamp',
-    check: false,
-  },
-];
+
 const RegistrationCategory = ({navigation}: {navigation: any}) => {
-  const [category, setcategory] = useState(categoryList);
+  const [groupValue, setGroupValue] = useState([]);
+
   return (
     <NativeBaseProvider>
       <Box height={'100%'}>
         <Box flex={1} width={'100%'} />
         <Box p={5} bg={'#fff'}>
-          {/* <FormControl> */}
+          <Text>쓰레기통 이름</Text>
           <Input size="lg" placeholder="lg Input" marginY={2} />
-          <FlatList
-            numColumns={2}
-            data={category}
-            renderItem={({item}) => (
-              <CheckInput name={item.name} checked={item.check} />
-            )}
+          <CategotyCheckbox
+            groupValue={groupValue}
+            setGroupValue={setGroupValue}
           />
-          {/* </FormControl> */}
-
           <BasicButton
             onPress={() => {
               navigation.navigate('RegistrationInfo');
@@ -103,13 +34,5 @@ const RegistrationCategory = ({navigation}: {navigation: any}) => {
     </NativeBaseProvider>
   );
 };
-const CheckInput = ({name, checked}: {name: string; checked: boolean}) => {
-  return (
-    <Box flex={1} paddingY={2}>
-      <Checkbox value={name} defaultIsChecked={checked}>
-        {name}
-      </Checkbox>
-    </Box>
-  );
-};
+
 export default RegistrationCategory;
