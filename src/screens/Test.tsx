2f130b788c88b1885eb335f096d5f9d335cd2ccd
sumi.byun <selina2000@naver.com>
2022-05-11 02:42:38 +0900

    chore : camera 맞는 예시 테스트 완
    https://react-native-camera.github.io/react-native-camera/docs/rncamera

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -8,14 +8,23 @@ import {
 } from 'react-native';
 import {RNCamera} from 'react-native-camera';
 
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>Waiting</Text>
+  </View>
+);
+
 class ExampleApp extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
         <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
           style={styles.preview}
           type={RNCamera.Constants.Type.back}
           flashMode={RNCamera.Constants.FlashMode.on}
@@ -30,28 +39,36 @@ class ExampleApp extends PureComponent {
             message: 'We need your permission to use your audio',
             buttonPositive: 'Ok',
             buttonNegative: 'Cancel',
+          }}>
+          {({camera, status, recordAudioPermissionStatus}) => {
+            if (status !== 'READY') {
+              return <PendingView />;
+            }
+            return (
+              <View
+                style={{
+                  flex: 0,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                }}>
+                <TouchableOpacity
+                  onPress={() => this.takePicture(camera)}
+                  style={styles.capture}>
+                  <Text style={{fontSize: 14}}> SNAP </Text>
+                </TouchableOpacity>
+              </View>
+            );
           }}
-          onGoogleVisionBarcodesDetected={({barcodes}) => {
-            console.log(barcodes);
-          }}
-        />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-          <TouchableOpacity
-            onPress={this.takePicture.bind(this)}
-            style={styles.capture}>
-            <Text style={{fontSize: 14}}> SNAP </Text>
-          </TouchableOpacity>
-        </View>
+        </RNCamera>
       </View>
     );
   }
 
-  takePicture = async () => {
-    if (this.camera) {
-      const options = {quality: 0.5, base64: true};
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
+  takePicture = async function (camera) {
+    const options = {quality: 0.5, base64: true};
+    const data = await camera.takePictureAsync(options);
+    //  eslint-disable-next-line
+    console.log(data.uri);
   };
 }
 
@@ -76,4 +93,5 @@ const styles = StyleSheet.create({
     margin: 20,
   },
 });
+
 export default ExampleApp;
