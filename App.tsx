e391a37f14eeafb8678bfec02acec6b613c5dc0f
Byeongmin Jeon <qudals7613@gmail.com>
2022-05-25 00:34:55 +0900

    tesing library setting

--- App.tsx
+++ App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import {RecoilRoot} from 'recoil';
 import {NativeBaseProvider} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from '~/navigations/MainNavigation';
 
 function App() {
   return (
-    <RecoilRoot>
-      <NativeBaseProvider>
-        <NavigationContainer>
-          <MainNavigation />
-        </NavigationContainer>
-      </NativeBaseProvider>
-    </RecoilRoot>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <MainNavigation />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
