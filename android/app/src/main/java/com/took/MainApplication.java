71f91e949611c42b0a369bf366c0a378f750b917
sumi.byun <selina2000@naver.com>
2022-05-31 21:24:27 +0900

    chore : firestore 세팅

--- android/app/src/main/java/com/took/MainApplication.java
+++ android/app/src/main/java/com/took/MainApplication.java
@@ -14,6 +14,7 @@ import com.took.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import androidx.multidex.MultiDexApplication; 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
