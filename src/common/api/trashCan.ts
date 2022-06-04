8e12497477dd153586b68130123dcec7bd6de94f
jeonbyeongmin <qudals7613@gmail.com>
2022-06-05 01:42:44 +0900

    fix : firestore trashCan property

--- src/common/api/trashCan.ts
+++ src/common/api/trashCan.ts
@@ -10,14 +10,17 @@ export async function getStaredTrashCans() {
   try {
     const userDoc = await users.doc(uid).get();
     const userData = userDoc.data();
-    const stars = userData?.stars;
+    const stars: string[] = userData?.stars;
 
     const staredTrashCans: ITrashCanInfo[] = [];
 
     for (const staredItemId of stars) {
       const trashCanDoc = await trashCans.doc(staredItemId).get();
       const trashCanData = trashCanDoc.data();
-      staredTrashCans.push(trashCanData as ITrashCanInfo);
+      staredTrashCans.push({
+        ...trashCanData,
+        id: staredItemId,
+      } as ITrashCanInfo);
     }
 
     return staredTrashCans;
@@ -36,7 +39,10 @@ export async function getRegisterTrashCans() {
     for (const registedItemId of registed) {
       const trashCanDoc = await trashCans.doc(registedItemId).get();
       const trashCanData = trashCanDoc.data();
-      registedTrashCans.push(trashCanData as ITrashCanInfo);
+      registedTrashCans.push({
+        ...trashCanData,
+        id: registedItemId,
+      } as ITrashCanInfo);
     }
 
     return registedTrashCans;
