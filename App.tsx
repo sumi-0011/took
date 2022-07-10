f1808194a594519770e33231cfa1bcc237d966f2
jeonbyeongmin <qudals7613@gmail.com>
2022-07-10 23:15:33 +0900

    커스텀 colorScheme 적용

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import {NativeBaseProvider} from 'native-base';
+import {extendTheme, NativeBaseProvider} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from '@navigations/MainNavigation';
 import {RecoilRoot} from 'recoil';
+
 function App() {
+  const theme = extendTheme({
+    components: {
+      Button: {
+        variants: {
+          light: ({colorScheme}: any) => {
+            return {
+              bg: `${colorScheme}.500`,
+              _pressed: {bg: `${colorScheme}.600`},
+            };
+          },
+        },
+      },
+    },
+  });
+
   return (
     <RecoilRoot>
-      <NativeBaseProvider>
+      <NativeBaseProvider theme={theme}>
         <NavigationContainer>
           <MainNavigation />
         </NavigationContainer>
