160c3f91169cd2a17cfa6227d81733659463ac10
sumi.byun <selina2000@naver.com>
2022-07-13 21:39:50 +0900

    🔧 상수 폴더 절대경로 설정
    @constants로 설정 안되는 이유를 모르겠다.

--- babel.config.js
+++ babel.config.js
@@ -25,8 +25,10 @@ module.exports = {
           '@navigations': './src/navigations',
           '@recoil': './src/recoil',
           '@screens': './src/screens',
+          // NOTE : types 만 이렇게 해둔 이유는 뭐죵
           types: './src/types',
           '@utils': './src/utils',
+          '@constants': './scr/constants',
         },
       },
     ],
