583be052ab690d1438ac1980c623844a70f78eca
sumi.byun <selina2000@naver.com>
2022-07-15 00:27:53 +0900

    ♻️ 파일 이름 수정, 리코일 더미데이터 제거

--- src/components/ScreenHeader/index.tsx
+++ src/components/ScreenHeader/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 interface ScreenHeaderProps {
   text: string;
 }
-
+//note : 회원가입, 로그인에서 사용, 꼭 이렇게 컴포넌트로 만들어서 사용해야할까요?
 function ScreenHeader({text}: ScreenHeaderProps) {
   return (
     <Text fontSize="24px" fontWeight="bold" margin={10}>
