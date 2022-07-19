374941947311c3e377607ad2c41126448741b20a
jeonbyeongmin <qudals7613@gmail.com>
2022-07-19 23:08:12 +0900

    `MapModal` 컴포넌트 최적화

--- src/screens/MapScreen/MapModal.tsx
+++ src/screens/MapScreen/MapModal.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Box, HStack, Image, Text} from 'native-base';
 import {useRecoilState} from 'recoil';
 import {updateStar, getUser, updateLastTookTime} from '@api/userAPI';
@@ -11,92 +11,101 @@ import BadgeList from '@components/BadgeList';
 import TOOKBtn from '@components/TookButton';
 import IconBtn from './IconBtn';
 import {isLoggedIn} from '@api/fireAuthAPI';
+import CenterSpinner from '@components/CenterSpinner';
 
 interface MapModalProps {
   currentTrashCanID: string;
 }
 
 function MapModal({currentTrashCanID}: MapModalProps) {
+  const [userInfo, setUserInfo] = useRecoilState<UserInfoType>(UserState);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isStar, setIsStar] = useState<boolean>(false);
   const [isTook, setIsTook] = useState<boolean>(false);
+  const [selectTrashCanInfo, setSelectTrashCanInfo] =
+    useState<TrashCanInfoType>();
 
-  const [userInfo, setUserInfo] = useRecoilState<UserInfoType>(UserState);
-  const [selectTCInfo, setSelectTCInfo] = useState<TrashCanInfoType>();
-
-  const fetchData = async () => {
+  const fetchMapModalData = useCallback(async () => {
     try {
-      const response = await getUser();
-      response && setUserInfo(response);
+      setIsLoading(true);
+
+      const [user, trashCan] = await Promise.all([
+        getUser(),
+        getTrashCan(currentTrashCanID),
+      ]);
+
+      setUserInfo(user);
+      setSelectTrashCanInfo(trashCan);
+
+      const target = user.stars.find(star => star === currentTrashCanID);
+
+      if (target) {
+        setIsStar(true);
+      } else {
+        setIsStar(false);
+      }
     } catch (error) {
       console.log(error);
     }
-  };
+
+    setIsLoading(false);
+  }, [currentTrashCanID, setUserInfo]);
 
   useEffect(() => {
-    getTrashCan(currentTrashCanID)
-      .then(res => {
-        setSelectTCInfo(res);
-      })
-      .catch(error => console.log('getTrashCan error: ', error));
-  }, [currentTrashCanID]);
+    fetchMapModalData();
+  }, [fetchMapModalData]);
 
-  useEffect(() => {
-    const index = userInfo?.stars.findIndex(ele => ele === currentTrashCanID);
-    index !== -1 ? setIsStar(true) : setIsStar(false);
-  }, [currentTrashCanID, userInfo?.stars]);
+  // useEffect(() => {
+  //   if (userInfo) {
+  //     const currentDate = new Date();
+  //     const diffTime =
+  //       (currentDate.getTime() - userInfo.lastTookTime.getTime()) /
+  //       (1000 * 60 * 60);
+  //     diffTime >= 3 ? setIsTook(true) : setIsTook(false);
+  //   }
+  // }, [userInfo]);
 
-  useEffect(() => {
-    if (!userInfo) {
-      return;
-    }
-    const currentDate = new Date();
-    const diffTime =
-      (currentDate.getTime() - userInfo.lastTookTime.getTime()) /
-      (1000 * 60 * 60);
-    diffTime >= 3 ? setIsTook(true) : setIsTook(false);
-  }, [userInfo]);
-
-  const updateStarApi = async (id: string, stars: string[]) => {
-    try {
-      await updateStar(id, stars);
-      fetchData();
-      setIsStar(!isStar);
-    } catch (error) {
-      console.log('updateStar error: ', error);
-    }
-  };
-
-  const handleStarClick = () => {
+  const handleStarClick = useCallback(async () => {
     if (!isLoggedIn) {
       console.log('로그인이 필요합니다.');
-      return;
-    }
-
-    const index = userInfo?.stars.findIndex(ele => ele === currentTrashCanID);
-    if (index === -1) {
-      // 추가
-      updateStarApi(userInfo.uid, [...userInfo.stars, currentTrashCanID]);
     } else {
-      // 제거
-      const filterStars = userInfo.stars.filter(
-        star => star !== currentTrashCanID,
-      );
-      updateStarApi(userInfo.uid, filterStars);
+      const target = userInfo?.stars.find(star => star === currentTrashCanID);
+
+      if (!target) {
+        try {
+          await updateStar([...userInfo.stars, currentTrashCanID]);
+          await fetchMapModalData();
+        } catch (error) {
+          console.log('updateStar error: ', error);
+        }
+      } else {
+        const filterStars = userInfo.stars.filter(
+          star => star !== currentTrashCanID,
+        );
+
+        try {
+          await updateStar(filterStars);
+          await fetchMapModalData();
+        } catch (error) {
+          console.log('updateStar error: ', error);
+        }
+      }
     }
-  };
+  }, [currentTrashCanID, fetchMapModalData, userInfo.stars]);
 
-  const handleTookBtnClick = () => {
-    updateLastTookTime(userInfo.uid, userInfo.tookCnt)
-      .then(() => fetchData())
-      .catch(error => console.log('updateLastTookTime error: ', error));
-  };
+  const handleTookBtnClick = useCallback(() => {
+    console.log('툭 버리기 클릭');
+    // updateLastTookTime(userInfo.tookCnt)
+    //   .then(() => fetchData())
+    //   .catch(error => console.log('updateLastTookTime error: ', error));
+  }, []);
 
-  const handleReportClick = () => {
-    // TODO : 신고하기 버튼 클릭
+  const handleReportClick = useCallback(() => {
     console.log('신고하기 버튼 클릭');
-  };
+  }, []);
 
-  return selectTCInfo ? (
+  return (
     <Box
       borderTopRadius="20"
       p={5}
@@ -104,66 +113,68 @@ function MapModal({currentTrashCanID}: MapModalProps) {
       position={'absolute'}
       bottom="0"
       bgColor={'#fff'}>
-      <Box
-        flexDirection={'row'}
-        paddingY={2}
-        borderBottomWidth="1"
-        borderColor="coolGray.200">
-        {/* trach can info */}
-        <Box flex={1}>
-          <Text fontSize="lg" bold>
-            {selectTCInfo.name}
-          </Text>
-          {/* NOTE : addresss 자리  */}
-          {/* <Text fontSize="sm" color={'coolGray.500'}>
-            {address}
-          </Text> */}
-          <Box>
-            <BadgeList data={selectTCInfo.tags} />
+      {!isLoading ? (
+        <>
+          <Box
+            flexDirection={'row'}
+            paddingY={2}
+            borderBottomWidth="1"
+            borderColor="coolGray.200">
+            <Box flex={1}>
+              <Text fontSize="lg" bold>
+                {selectTrashCanInfo?.name}
+              </Text>
+              {/* NOTE : addresss 자리  */}
+              {/* <Text fontSize="sm" color={'coolGray.500'}>
+                {address}
+              </Text> */}
+              <Box>
+                <BadgeList data={selectTrashCanInfo?.tags ?? []} />
+              </Box>
+            </Box>
+            {/* trash can image */}
+            <Box w={100} h={'100%'}>
+              <Image
+                source={{
+                  uri:
+                    selectTrashCanInfo?.trashImage ??
+                    'http://www.solartodaymag.com/news/photo/201705/4484_3192_3220.jpg',
+                }}
+                alt="detail img"
+                width={100}
+                height={90}
+                borderRadius={10}
+              />
+            </Box>
           </Box>
-        </Box>
-        {/* trash can image */}
-        <Box w={100} h={'100%'}>
-          <Image
-            source={{
-              uri:
-                selectTCInfo.trashImage ??
-                'http://www.solartodaymag.com/news/photo/201705/4484_3192_3220.jpg',
-            }}
-            alt="detail img"
-            width={100}
-            height={90}
-            borderRadius={10}
+          <HStack paddingY={3}>
+            <IconBtn
+              text="MY TOOK"
+              icon={
+                isStar ? (
+                  <HearFilltIcon size={25} />
+                ) : (
+                  <HeartOutlineIcon size={25} />
+                )
+              }
+              onPress={handleStarClick}
+            />
+            <IconBtn
+              text="신고하기"
+              icon={<ReportIcon size={20} />}
+              onPress={handleReportClick}
+            />
+          </HStack>
+          <TOOKBtn
+            name=" TOOK 버리기"
+            isDisabled={!isTook}
+            onPress={handleTookBtnClick}
           />
-        </Box>
-      </Box>
-      {/* button list */}
-      <HStack paddingY={3}>
-        <IconBtn
-          text="MY TOOK"
-          icon={
-            isStar ? (
-              <HearFilltIcon size={25} />
-            ) : (
-              <HeartOutlineIcon size={25} />
-            )
-          }
-          onPress={handleStarClick}
-        />
-        <IconBtn
-          text="신고하기"
-          icon={<ReportIcon size={20} />}
-          onPress={handleReportClick}
-        />
-      </HStack>
-      <TOOKBtn
-        name=" TOOK 버리기"
-        isDisabled={!isTook}
-        onPress={handleTookBtnClick}
-      />
+        </>
+      ) : (
+        <CenterSpinner />
+      )}
     </Box>
-  ) : (
-    <Text>정보가없습니다.</Text>
   );
 }
 
