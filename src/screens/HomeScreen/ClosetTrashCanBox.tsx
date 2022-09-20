2db5c80c7d8bd6a8b9c8bade17dad599c440bf6e
sumi-0011 <selina2000@naver.com>
2022-09-20 22:08:30 +0900

    가장 가까운 쓰레기통 필터링 seleted box 구현

--- src/screens/HomeScreen/ClosetTrashCanBox.tsx
+++ src/screens/HomeScreen/ClosetTrashCanBox.tsx
@@ -1,87 +1,112 @@
 import {getTrashCans} from '@api/trashCanAPI';
 import useCurrentLocation from '@hooks/useCurrentLocation';
 import {getDistanceFromLatLonInKm} from '@utils/distance';
-import {Text, VStack} from 'native-base';
-import React, {useEffect, useState} from 'react';
+import {RECYCLE_FILTER_LIST} from 'constants/trashCanRegister';
+import {Box, CheckIcon, Select, Text, VStack} from 'native-base';
+import React, {useCallback, useEffect, useState} from 'react';
 import {TrashCanType} from 'types/TrashCanType';
 import BackgroundImage from './BackgroundImage';
 import PressableBox from './PressableBox';
 import TagList from './TagList';
 
 function ClosetTrashCanBox() {
-  const [closetTrashCan, setclosetTrashCan] = useState<TrashCanType>();
   const {location} = useCurrentLocation();
+
+  const [closetTrashCan, setclosetTrashCan] = useState<TrashCanType>();
+  const [seletedFilter, setSeletedFilter] = useState('');
+
+  const getClosetTrashCan = useCallback(
+    async (currentLocation: {latitude: number; longitude: number}) => {
+      try {
+        const trashCans = await getTrashCans();
+        let closetTrashCanInfo: {
+          distance: number;
+          id: string;
+        } = {
+          distance: Number.MAX_SAFE_INTEGER,
+          id: '',
+        };
+        trashCans.forEach(({id, coordinate, tags}) => {
+          if (seletedFilter && seletedFilter !== '전체') {
+            if (!tags.includes(seletedFilter)) {
+              return;
+            }
+          }
+
+          const {latitude, longitude} = coordinate;
+          const TrashCanBeteenDistance = getDistanceFromLatLonInKm(
+            currentLocation.latitude,
+            currentLocation.longitude,
+            latitude,
+            longitude,
+          );
+
+          if (closetTrashCanInfo.distance > TrashCanBeteenDistance) {
+            closetTrashCanInfo = {
+              distance: TrashCanBeteenDistance,
+              id,
+            };
+          }
+        });
+
+        const findClosetTrashCan = trashCans.filter(
+          trashCan => trashCan.id === closetTrashCanInfo.id,
+        )[0];
+
+        setclosetTrashCan(findClosetTrashCan);
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    },
+    [seletedFilter],
+  );
 
   useEffect(() => {
     location && getClosetTrashCan(location);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
-
-  const getClosetTrashCan = async (currentLocation: {
-    latitude: number;
-    longitude: number;
-  }) => {
-    try {
-      const trashCans = await getTrashCans();
-      let closetTrashCanInfo: {
-        distance: number;
-        id: string;
-      } = {
-        distance: Number.MAX_SAFE_INTEGER,
-        id: '',
-      };
-
-      trashCans.forEach(({id, coordinate}) => {
-        const {latitude, longitude} = coordinate;
-        const TrashCanBeteenDistance = getDistanceFromLatLonInKm(
-          currentLocation.latitude,
-          currentLocation.longitude,
-          latitude,
-          longitude,
-        );
-
-        if (closetTrashCanInfo.distance > TrashCanBeteenDistance) {
-          closetTrashCanInfo = {
-            distance: TrashCanBeteenDistance,
-            id,
-          };
-        }
-      });
-
-      const findClosetTrashCan = trashCans.filter(
-        trashCan => trashCan.id === closetTrashCanInfo.id,
-      )[0];
-
-      setclosetTrashCan(findClosetTrashCan);
-    } catch (error) {
-      console.log('error: ', error);
-    }
-  };
+  }, [getClosetTrashCan, location]);
 
   return (
-    <PressableBox height="48" onPress={() => console.log('clicked')}>
-      {closetTrashCan ? (
-        <BackgroundImage img={closetTrashCan?.trashImage}>
-          <VStack p={5} flex={1} justifyContent="space-between">
-            <Text fontSize="xl" color="white" bold>
-              가장 가까운 쓰레기통
-            </Text>
-            <VStack alignItems={'flex-end'} space="1">
-              <Text color="white" fontSize="lg">
-                {closetTrashCan?.name}
+    <>
+      <Box marginBottom={3}>
+        <Select
+          selectedValue={seletedFilter}
+          defaultValue={'all'}
+          accessibilityLabel="Choose filtering"
+          placeholder="Choose filtering"
+          _selectedItem={{
+            endIcon: <CheckIcon size="5" />,
+          }}
+          mt={1}
+          onValueChange={itemValue => setSeletedFilter(itemValue)}>
+          {RECYCLE_FILTER_LIST.map(({key, name}) => (
+            <Select.Item key={key} label={name} value={name} />
+          ))}
+        </Select>
+      </Box>
+      <PressableBox height="48" onPress={() => console.log('clicked')}>
+        {closetTrashCan ? (
+          <BackgroundImage img={closetTrashCan.trashImage}>
+            <VStack p={5} flex={1} justifyContent="space-between">
+              <Text fontSize="xl" color="white" bold>
+                가장 가까운 쓰레기통
               </Text>
-              <TagList
-                color="white"
-                fontSize="md"
-                list={closetTrashCan?.tags ?? []}
-              />
+              <VStack alignItems={'flex-end'} space="1">
+                <Text color="white" fontSize="lg">
+                  {closetTrashCan.name}
+                </Text>
+                <TagList
+                  color="white"
+                  fontSize="md"
+                  list={closetTrashCan.tags ?? []}
+                />
+              </VStack>
             </VStack>
-          </VStack>
-        </BackgroundImage>
-      ) : (
-        <Text>가까운 쓰레기통이 없습니다. </Text>
-      )}
-    </PressableBox>
+          </BackgroundImage>
+        ) : (
+          <Text>가까운 쓰레기통이 없습니다. </Text>
+        )}
+      </PressableBox>
+    </>
   );
 }
 
